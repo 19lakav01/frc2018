ac783d08ae5c35e93ed9617640434053b5990d2f
GabriellaYang <34780010+GabriellaYang@users.noreply.github.com>
2018-01-17 16:43:58 -0800

    Update Robot.java
    Clean up

--- src/org/usfirst/frc/team6662/robot/Robot.java
+++ src/org/usfirst/frc/team6662/robot/Robot.java
@@ -13,27 +13,24 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
-
 public class Robot extends TimedRobot {
 	
 	public static Drivetrain drivetrain;
 	public static OI oi;
 	Command joystickTankDrive;
+	
 	@Override
 	public void robotInit() {
 		oi = new OI();
 		drivetrain = new Drivetrain();
 		joystickTankDrive = new JoystickTankDrive();
-		
 	}
-
 
 	@Override
 	public void teleopInit() {
 		joystickTankDrive.start();
 	}
 
-	
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
