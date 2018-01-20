24d2cdc6262b3f79f81ff4ada5cfac72193c7478
GabriellaYang <34780010+GabriellaYang@users.noreply.github.com>
2018-01-20 14:39:22 -0800

    Update RobotMap.java
    Added in values for the solenoid a

--- src/org/usfirst/frc/team6662/robot/RobotMap.java
+++ src/org/usfirst/frc/team6662/robot/RobotMap.java
@@ -2,11 +2,27 @@
 package org.usfirst.frc.team6662.robot;
 
 public class RobotMap {
+	//Things for the Drive
 	public static final int JOYSTICK_PORT = 0;
 	 public static final int TOP_LEFT_MOTOR    = 0, 
 			 				BOTTOM_LEFT_MOTOR  = 1,
 			 				TOP_RIGHT_MOTOR    = 2, 
 			 				BOTTOM_RIGHT_MOTOR = 3;
-	 public static final int ULTRASONIC_ECHO_PULSE_OUTPUT = 1,
-							ULTRASONIC_TRIGGER_PULSE_INPUT = 1;
+	 //THings for the encoders
+	 public static final int ENCODER_LEFT_A = 0;
+	 public static final int ENCODER_RIGHT_A = 0;
+	 public static final int ENCODER_LEFT_B = 0;
+	 public static final int ENCODER_RIGHT_B = 0;
+	 
+	//Things for the Pneumatics system
+	
+	//Channels for double solenoid for grabber?
+	public static final int SOLE_CHANNEL_A = 0;
+	public static final int SOLE_CHANNEL_B = 1;
+	
+	// Channels for single solenoids
+	public static final int SSOLE_CHANNEL = 3;
+	
+	 
 }
+
