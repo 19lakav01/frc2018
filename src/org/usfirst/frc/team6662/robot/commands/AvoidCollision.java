b23a3d2951c9e29a9b9f423e332b53eb63ce86a7
GabriellaYang <34780010+GabriellaYang@users.noreply.github.com>
2018-01-21 15:45:29 -0800

    Update AvoidCollision.java
    Added comment, got rid of single lines

--- src/org/usfirst/frc/team6662/robot/commands/AvoidCollision.java
+++ src/org/usfirst/frc/team6662/robot/commands/AvoidCollision.java
@@ -4,15 +4,22 @@ import org.usfirst.frc.team6662.robot.Robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 public class AvoidCollision extends Command {
-	public AvoidCollision() {requires(Robot.drivetrain);}
+	public AvoidCollision()
+	{
+		requires(Robot.drivetrain);
+	}
 	@Override
-	protected void initialize() {}
+	protected void initialize() {
+	}
 	@Override
 	protected void execute() {
 		while ((Robot.drivetrain.isValidRange() == true && Robot.drivetrain.getInchRange() <= 32) && Robot.isAutonomous== true) {
 			//if it detects something in autonomous
 			Robot.drivetrain.drive(0,0);
 			Timer.delay(2);
+			// OK, now we just need to figure out how to make this specific to robots, maybe see if we can make the code detect
+			//accelleration so it doesn't stop when at switch?
+			
 		}
 	}
 	@Override
