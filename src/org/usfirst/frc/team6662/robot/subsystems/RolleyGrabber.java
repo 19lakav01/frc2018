9653ab2d906bf3ff6e50de50bc83ef29f35acf25
GabriellaYang <34780010+GabriellaYang@users.noreply.github.com>
2018-01-21 13:59:54 -0800

    Create RolleyGrabber.java
    Oops, put new file in subsystem

---
+++ src/org/usfirst/frc/team6662/robot/subsystems/RolleyGrabber.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team6662.robot.subsystems;
+
+import org.usfirst.frc.team6662.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class RolleyGrabber extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	DoubleSolenoid dSolenoid = new DoubleSolenoid(RobotMap.SOLE_CHANNEL_A,RobotMap. SOLE_CHANNEL_B);
+	Solenoid sSolenoid = new Solenoid(RobotMap.SSOLE_CHANNEL);
+//All methods for rolley grabber
+	public void setPush()
+	{
+		dSolenoid.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	
+	public void setReverse()
+	{
+		dSolenoid.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	public void setOff()
+	{
+		dSolenoid.set(DoubleSolenoid.Value.kOff);
+	}
+	//end of rolley grabber methods
+	
+	public void setSingOn()
+	{
+		sSolenoid.set(true);
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
