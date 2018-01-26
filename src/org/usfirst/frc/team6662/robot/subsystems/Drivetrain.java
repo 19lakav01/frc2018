6570024f503d8fca56359207e4b1314b30129941
GabriellaYang <34780010+GabriellaYang@users.noreply.github.com>
2018-01-26 15:43:45 -0800

    Update Drivetrain.java
    Got rid of gyro for now

--- src/org/usfirst/frc/team6662/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team6662/robot/subsystems/Drivetrain.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team6662.robot.subsystems;
 
-import edu.wpi.first.wpilibj.AnalogGyro;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -13,17 +13,11 @@ public class Drivetrain extends Subsystem {
 	Spark m_frontLeft = new Spark(RobotMap.TOP_LEFT_MOTOR);
 	Spark m_rearLeft = new Spark(RobotMap.BOTTOM_LEFT_MOTOR);
 	SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
-
 	Spark m_frontRight = new Spark(RobotMap.TOP_RIGHT_MOTOR);
 	Spark m_rearRight = new Spark(RobotMap.BOTTOM_RIGHT_MOTOR);
 	SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
 	DifferentialDrive drivetrain = new DifferentialDrive(m_left, m_right);
-	
 	DoubleSolenoid shifter = new DoubleSolenoid(0, 1);
-		   
-	AnalogGyro gyro = new AnalogGyro(RobotMap.GYRO_ANALOG);
-	
-	
 	
 		  // Ultrasonic sonic = new Ultrasonic(RobotMap.ULTRASONIC_ECHO_PULSE_OUTPUT,
 					//RobotMap.ULTRASONIC_TRIGGER_PULSE_INPUT);
@@ -37,8 +31,7 @@ public class Drivetrain extends Subsystem {
 	
 	public Drivetrain() {
 		super("Drivetrain");
-		gyro.initGyro();
-		gyro.calibrate();
+		
 	}
 //	public boolean isValidRange() {return sonic.isRangeValid();}
 	//public double getInchRange() {return sonic.getRangeInches();}
@@ -46,6 +39,6 @@ public class Drivetrain extends Subsystem {
 	//make sur to fiish ode before pushing into git
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
+		
 	}
 }
