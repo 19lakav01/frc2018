e80e3f1c893e55d28133975a0ed389d9023653a1
bkong <bkong@brians-mbp.vvms.site.mdusd.org>
2018-01-26 15:39:16 -0800

    Robot stops moving as long as something is within

--- src/org/usfirst/frc/team6662/robot/commands/AvoidCollision.java
+++ src/org/usfirst/frc/team6662/robot/commands/AvoidCollision.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team6662.robot.commands;
 
 import org.usfirst.frc.team6662.robot.Robot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AvoidCollision extends Command {
@@ -13,15 +12,12 @@ public class AvoidCollision extends Command {
 	}
 	@Override
 	protected void execute() {
-		//while ((Robot.drivetrain.isValidRange() == true && Robot.drivetrain.getInchRange() <= 32)) {
+		while ((Robot.drivetrain.isValidRange() == true && Robot.drivetrain.getInchRange() <= 32)) {
 			//if it detects something in autonomous
-			//Robot.drivetrain.drive(0,0);
-			//Timer.delay(2);
-			// OK, now we just need to figure out how to make this specific to robots, maybe see if we can make the code detect
-			//accelleration so it doesn't stop when at switch?
-			
+			Robot.drivetrain.drive(0,0);
 		}
 	}
+	
 	@Override
 	protected boolean isFinished() {
 		return true;
